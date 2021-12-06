008f64890bb9f4efded59490edc065fc2737fb9b
Niall Cooling <niall.cooling@gmail.com>
2021-12-06 12:24:36 +0000

    added debug and release build to cmake

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+USAGE="Usage: $(basename $0) [-v | --verbose] [ reset | clean | debug | release ]"
+
+CMAKE=cmake
+BUILD=./build
+TYPE=DEBUG
+BUILD_DIR=$BUILD/debug
+CLEAN=
+RESET=
+VERBOSE=
+
+for arg; do
+  case "$arg" in
+    --help|-h)    echo $USAGE; exit 0;;
+    -v|--verbose) VERBOSE='VERBOSE=1'  ;;
+    debug)        TYPE=DEBUG;   BUILD_DIR=$BUILD/debug ;;
+    release)      TYPE=RELEASE; BUILD_DIR=$BUILD/release ;;
+    clean)        CLEAN=1  ;;
+    reset)        RESET=1 ;;
+    *)            echo -e "unknown option $arg\n$USAGE" >&2;  exit 1 ;;
+  esac
+done
+
+[[ -n $RESET && -d $BUILD_DIR ]] && rm -rf $BUILD_DIR
+    
+$CMAKE -S . -B $BUILD_DIR --warn-uninitialized -DCMAKE_BUILD_TYPE=$TYPE
+
+[[ -n $CLEAN ]] && $CMAKE --build $BUILD_DIR --target clean
+
+$CMAKE --build $BUILD_DIR --no-print-directory -- $VERBOSE
