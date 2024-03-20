0a1a1a867cd0dcc47429f99613f613aa9bcda254
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-03-20 12:55:30 +0100

    udpate to v9

--- README.md
+++ README.md
@@ -2,19 +2,12 @@
 
 The [LVGL](https://github.com/lvgl/lvgl) is written mainly for microcontrollers and embedded systems however you can run the library **on your PC** as well without any embedded hardware. The code written on PC can be simply copied when your are using an embedded system.
 
-Using a PC simulator instead of an embedded hardware has several advantages:
-* **Costs $0** because you don't have to buy or design PCB
-* **Fast** because you don't have to design and manufacture PCB
-* **Collaborative** because any number of developers can work in the same environment
-* **Developer friendly** because much easier and faster to debug on PC
-
 ## Requirements
 This project is configured for [VSCode](https://code.visualstudio.com) and only tested on Linux, although this may work on OSx or WSL. It requires a working version of GCC, GDB and make in your path.
 
 To allow debugging inside VSCode you will also require a GDB [extension](https://marketplace.visualstudio.com/items?itemName=webfreak.debug) or other suitable debugger. All the requirements have been pre-configured in the [.workspace](simulator.code-workspace) file (simply open the project by doubleclick on this file).
 
-The project can use **SDL** or **X11** as LVGL display driver for lowlevel graphics/mouse/keyboard support. This can be defined in the [Makefile](Makefile#L8).
-Please make sure the selected library is installed in the system (check [Install graphics driver](#install-graphics-driver)).
+The project can use **SDL** but it can be easily relaced by any other built-in LVGL dirvers.
 
 ## Usage
 
@@ -27,8 +20,8 @@ git clone --recursive https://github.com/lvgl/lv_port_pc_vscode
 ```
 
 ### Install graphics driver
-The project can use **SDL** or **X11** as LVGL display driver. This can be selected in the [Makefile](Makefile#L8).
-Please make sure the used library is installed in the system:
+ 
+Please make sure **SDL**  is installed in the system:
 
 #### Install SDL
 You can download SDL from https://www.libsdl.org/
@@ -36,12 +29,6 @@ You can download SDL from https://www.libsdl.org/
 On on Linux you can install it via terminal:
 ```bash
 sudo apt-get update && sudo apt-get install -y build-essential libsdl2-dev
-```
-
-#### Install X11
-On on Linux you can install it via terminal:
-```bash
-sudo apt-get update && sudo apt-get install -y libx11-dev
 ```
 
 ### Optional library
@@ -65,22 +52,3 @@ git checkout release/6.0
 make
 sudo make install
 ```
-
-And then remove all the comments in the `Makefile` on `INC` and `LDLIBS` lines. \
-They should be for **SDL**:
-```Makefile
-INC    := -I./ui/simulator/inc/ -I./ -I./lvgl/ -I/usr/include/freetype2 -L/usr/local/lib
-LDLIBS := -lSDL2 -lm -lfreetype -lavformat -lavcodec -lavutil -lswscale -lm -lz -lpthread
-```
-They should be for **X11**:
-```Makefile
-INC    := -I./ui/simulator/inc/ -I./ -I./lvgl/ -I/usr/include/freetype2 -L/usr/local/lib
-LDLIBS := -lX11 -lm -lfreetype -lavformat -lavcodec -lavutil -lswscale -lm -lz -lpthread
-```
-
-### Setup
-To allow custom UI code an `lv_conf.h` file placed at `ui/simulator/inc` will automatically override this projects lv_conf.h file. By default code under `ui` is ignored so you can reuse this repository for multiple projects. You will need to place a call from `main.c` to your UI's entry function.
-
-To build and debug, press F5. You should now have your UI displayed in a new window and can access all the debug features of VSCode through GDB.
-
-To allow temporary modification between simulator and device code, a SIMULATOR=1 define is added globally.
--- main/src/main.c
+++ main/src/main.c
@@ -11,21 +11,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include "lv_drv_conf.h"
 #include "lvgl/lvgl.h"
 #include "lvgl/examples/lv_examples.h"
 #include "lvgl/demos/lv_demos.h"
-#if USE_SDL
-  #define SDL_MAIN_HANDLED /*To fix SDL's "undefined reference to WinMain" issue*/
-  #include <SDL2/SDL.h>
-  #include "lv_drivers/sdl/sdl.h"
-#elif USE_X11
-  #include "lv_drivers/x11/x11.h"
-#endif
-// #include "lv_drivers/display/monitor.h"
-// #include "lv_drivers/indev/mouse.h"
-// #include "lv_drivers/indev/keyboard.h"
-// #include "lv_drivers/indev/mousewheel.h"
 
 /*********************
  *      DEFINES
@@ -38,17 +26,13 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static void hal_init(void);
-static void hal_deinit(void);
-static void* tick_thread(void *data);
+static lv_display_t * hal_init(int32_t w, int32_t h);
 
 /**********************
  *  STATIC VARIABLES
  **********************/
-static pthread_t thr_tick;    /* thread */
-static bool end_tick = false; /* flag to terminate thread */
 
-/**********************
+/********************** 
  *      MACROS
  **********************/
 
@@ -75,92 +59,6 @@ static bool end_tick = false; /* flag to terminate thread */
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/
-#if 0
-static void user_image_demo()
-{
-  lv_obj_t * img = lv_gif_create(lv_scr_act());
-  lv_gif_set_src(img, "A:lvgl/examples/libs/gif/bulb.gif");
-  lv_obj_align(img, LV_ALIGN_BOTTOM_RIGHT, -20, -20);
-
-  lv_color_t bg_color = lv_palette_lighten(LV_PALETTE_LIGHT_BLUE, 5);
-    lv_color_t fg_color = lv_palette_darken(LV_PALETTE_BLUE, 4);
-
-    lv_obj_t * qr = lv_qrcode_create(lv_scr_act(), 150, fg_color, bg_color);
-
-    /*Set data*/
-    const char * data = "https://lvgl.io";
-    lv_qrcode_update(qr, data, strlen(data));
-    lv_obj_center(qr);
-
-    /*Add a border with bg_color*/
-    lv_obj_set_style_border_color(qr, bg_color, 0);
-    lv_obj_set_style_border_width(qr, 5, 0);
-
-    /*Create a font*/
-    static lv_ft_info_t info;
-    /*FreeType uses C standard file system, so no driver letter is required.*/
-    info.name = "./lvgl/examples/libs/freetype/Lato-Regular.ttf";
-    info.weight = 24;
-    info.style = FT_FONT_STYLE_NORMAL;
-    info.mem = NULL;
-    if(!lv_ft_font_init(&info)) {
-        LV_LOG_ERROR("create failed.");
-    }
-
-    /*Create style with the new font*/
-    static lv_style_t style;
-    lv_style_init(&style);
-    lv_style_set_text_font(&style, info.font);
-    lv_style_set_text_align(&style, LV_TEXT_ALIGN_CENTER);
-
-    /*Create a label with the new style*/
-    lv_obj_t * label = lv_label_create(lv_scr_act());
-    lv_obj_add_style(label, &style, 0);
-    lv_label_set_text(label, "Hello world\nI'm a font created with FreeType");
-    lv_obj_set_pos(label, 10, 10);
-
-    lv_obj_t *  img1 = lv_img_create(lv_scr_act());
-    /* Assuming a File system is attached to letter 'A'
-     * E.g. set LV_USE_FS_STDIO 'A' in lv_conf.h */
-    lv_img_set_src(img1, "A:lvgl/examples/libs/png/wink.png");
-    lv_obj_align(img1, LV_ALIGN_LEFT_MID, 20, 0);
-
-    lv_obj_t * wp;
-
-    wp = lv_img_create(lv_scr_act());
-    /* Assuming a File system is attached to letter 'A'
-     * E.g. set LV_USE_FS_STDIO 'A' in lv_conf.h */
-    lv_img_set_src(wp, "A:lvgl/examples/libs/sjpg/small_image.sjpg");
-    lv_obj_align(wp, LV_ALIGN_RIGHT_MID, -20, 0);
-
-    lv_obj_t * img2 = lv_img_create(lv_scr_act());
-    /* Assuming a File system is attached to letter 'A'
-     * E.g. set LV_USE_FS_STDIO 'A' in lv_conf.h */
-    lv_img_set_src(img2, "A:lvgl/examples/libs/sjpg/lv_example_jpg.jpg");
-    //lv_obj_center(img);
-    lv_obj_align(img2, LV_ALIGN_TOP_RIGHT, -20, 20);
-
-    lv_obj_t * img3 = lv_img_create(lv_scr_act());
-    /* Assuming a File system is attached to letter 'A'
-     * E.g. set LV_USE_FS_STDIO 'A' in lv_conf.h */
-#if LV_COLOR_DEPTH == 32
-    lv_img_set_src(img3, "A:lvgl/examples/libs/bmp/example_32bit.bmp");
-#elif LV_COLOR_DEPTH == 16
-    lv_img_set_src(img, "A:lvgl/examples/libs/bmp/example_16bit.bmp");
-#endif
-    lv_obj_align(img3, LV_ALIGN_BOTTOM_MID, 0, -20);
-
-    lv_obj_t * img4 = lv_img_create(lv_scr_act());
-    lv_img_set_src(img4, "A:lvgl/examples/libs/ffmpeg/ffmpeg.png");
-    lv_obj_align(img4, LV_ALIGN_BOTTOM_LEFT, 20, -20);
-
-    lv_obj_t * player = lv_ffmpeg_player_create(lv_scr_act());
-    lv_ffmpeg_player_set_src(player, "./lvgl/examples/libs/ffmpeg/birds.mp4");
-    lv_ffmpeg_player_set_auto_restart(player, true);
-    lv_ffmpeg_player_set_cmd(player, LV_FFMPEG_PLAYER_CMD_START);
-    lv_obj_align(player, LV_ALIGN_TOP_MID, 0, 20);
-}
-#endif
 
 int main(int argc, char **argv)
 {
@@ -171,33 +69,9 @@ int main(int argc, char **argv)
   lv_init();
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  hal_init();
-
-//  lv_example_switch_1();
-//  lv_example_calendar_1();
-//  lv_example_btnmatrix_2();
-//  lv_example_checkbox_1();
-//  lv_example_colorwheel_1();
-//  lv_example_chart_6();
-//  lv_example_table_2();
-//  lv_example_scroll_2();
-//  lv_example_textarea_1();
-//  lv_example_msgbox_1();
-//  lv_example_dropdown_2();
-//  lv_example_btn_1();
-//  lv_example_scroll_1();
-//  lv_example_tabview_1();
-//  lv_example_tabview_1();
-//  lv_example_flex_3();
-//  lv_example_label_1();
+  hal_init(480, 272);
 
   lv_demo_widgets();
-//  lv_demo_keypad_encoder();
-//  lv_demo_benchmark();
-//  lv_demo_stress();
-//  lv_demo_music();
-
-//  user_image_demo();
 
   while(1) {
     /* Periodically call the lv_task handler.
@@ -206,7 +80,6 @@ int main(int argc, char **argv)
     usleep(5 * 1000);
   }
 
-  hal_deinit();
   return 0;
 }
 
@@ -218,133 +91,30 @@ int main(int argc, char **argv)
  * Initialize the Hardware Abstraction Layer (HAL) for the LVGL graphics
  * library
  */
-static void hal_init(void)
+static lv_display_t * hal_init(int32_t w, int32_t h)
 {
-  /* mouse input device */
-  static lv_indev_drv_t indev_drv_1;
-  lv_indev_drv_init(&indev_drv_1);
-  indev_drv_1.type = LV_INDEV_TYPE_POINTER;
 
-  /* keyboard input device */
-  static lv_indev_drv_t indev_drv_2;
-  lv_indev_drv_init(&indev_drv_2);
-  indev_drv_2.type = LV_INDEV_TYPE_KEYPAD;
+  lv_group_set_default(lv_group_create());
 
-  /* mouse scroll wheel input device */
-  static lv_indev_drv_t indev_drv_3;
-  lv_indev_drv_init(&indev_drv_3);
-  indev_drv_3.type = LV_INDEV_TYPE_ENCODER;
+  lv_display_t * disp = lv_sdl_window_create(w, h);
 
-  lv_group_t *g = lv_group_create();
-  lv_group_set_default(g);
+  lv_indev_t * mouse = lv_sdl_mouse_create();
+  lv_indev_set_group(mouse, lv_group_get_default());
+  lv_indev_set_display(mouse, disp);
+  lv_display_set_default(disp);
 
-  lv_disp_t *disp = NULL;
+  LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
+  lv_obj_t * cursor_obj;
+  cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
+  lv_image_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
+  lv_indev_set_cursor(mouse, cursor_obj);             /*Connect the image  object to the driver*/
 
-#if USE_SDL
-  /* Use the 'monitor' driver which creates window on PC's monitor to simulate a display*/
-  sdl_init();
+  lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
+  lv_indev_set_display(mousewheel, disp);
 
-  /*Create a display buffer*/
-  static lv_disp_draw_buf_t disp_buf1;
-  static lv_color_t buf1_1[MONITOR_HOR_RES * 100];
-  static lv_color_t buf1_2[MONITOR_HOR_RES * 100];
-  lv_disp_draw_buf_init(&disp_buf1, buf1_1, buf1_2, MONITOR_HOR_RES * 100);
+  lv_indev_t * kb = lv_sdl_keyboard_create();
+  lv_indev_set_display(kb, disp);
+  lv_indev_set_group(kb, lv_group_get_default());
 
-  /*Create a display*/
-  static lv_disp_drv_t disp_drv;
-  lv_disp_drv_init(&disp_drv); /*Basic initialization*/
-  disp_drv.draw_buf = &disp_buf1;
-  disp_drv.flush_cb = sdl_display_flush;
-  disp_drv.hor_res = MONITOR_HOR_RES;
-  disp_drv.ver_res = MONITOR_VER_RES;
-  disp_drv.antialiasing = 1;
-
-  disp = lv_disp_drv_register(&disp_drv);
-
-  /* Add the input device driver */
-  // mouse_init();
-  indev_drv_1.read_cb = sdl_mouse_read;
-
-  // keyboard_init();
-  indev_drv_2.read_cb = sdl_keyboard_read;
-
-  // mousewheel_init();
-  indev_drv_3.read_cb = sdl_mousewheel_read;
-
-#elif USE_X11
-  lv_x11_init("LVGL Simulator Demo", DISP_HOR_RES, DISP_VER_RES);
-
-  /*Create a display buffer*/
-  static lv_disp_draw_buf_t disp_buf1;
-  static lv_color_t buf1_1[DISP_HOR_RES * 100];
-  static lv_color_t buf1_2[DISP_HOR_RES * 100];
-  lv_disp_draw_buf_init(&disp_buf1, buf1_1, buf1_2, DISP_HOR_RES * 100);
-
-  /*Create a display*/
-  static lv_disp_drv_t disp_drv;
-  lv_disp_drv_init(&disp_drv);
-  disp_drv.draw_buf = &disp_buf1;
-  disp_drv.flush_cb = lv_x11_flush;
-  disp_drv.hor_res = DISP_HOR_RES;
-  disp_drv.ver_res = DISP_VER_RES;
-  disp_drv.antialiasing = 1;
-
-  disp = lv_disp_drv_register(&disp_drv);
-
-  /* Add the input device driver */
-  indev_drv_1.read_cb = lv_x11_get_pointer;
-  indev_drv_2.read_cb = lv_x11_get_keyboard;
-  indev_drv_3.read_cb = lv_x11_get_mousewheel;
-#endif
-  /* Set diplay theme */
-  lv_theme_t * th = lv_theme_default_init(disp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), LV_THEME_DEFAULT_DARK, LV_FONT_DEFAULT);
-  lv_disp_set_theme(disp, th);
-
-  /* Tick init */
-  end_tick = false;
-  pthread_create(&thr_tick, NULL, tick_thread, NULL);
-
-  /* register input devices */
-  lv_indev_t *mouse_indev = lv_indev_drv_register(&indev_drv_1);
-  lv_indev_t *kb_indev = lv_indev_drv_register(&indev_drv_2);
-  lv_indev_t *enc_indev = lv_indev_drv_register(&indev_drv_3);
-  lv_indev_set_group(kb_indev, g);
-  lv_indev_set_group(enc_indev, g);
-
-  /* Set a cursor for the mouse */
-  LV_IMG_DECLARE(mouse_cursor_icon);                   /*Declare the image file.*/
-  lv_obj_t * cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor*/
-  lv_img_set_src(cursor_obj, &mouse_cursor_icon);      /*Set the image source*/
-  lv_indev_set_cursor(mouse_indev, cursor_obj);        /*Connect the image  object to the driver*/
-}
-
-/**
- * Releases the Hardware Abstraction Layer (HAL) for the LVGL graphics library
- */
-static void hal_deinit(void)
-{
-  end_tick = true;
-  pthread_join(thr_tick, NULL);
-
-#if USE_SDL
-  // nop
-#elif USE_X11
-  lv_x11_deinit();
-#endif
-}
-
-/**
- * A task to measure the elapsed time for LVGL
- * @param data unused
- * @return never return
- */
-static void* tick_thread(void *data) {
-  (void)data;
-
-  while(!end_tick) {
-    usleep(5000);
-    lv_tick_inc(5); /*Tell LittelvGL that 5 milliseconds were elapsed*/
-  }
-
-  return NULL;
+  return disp;
 }
