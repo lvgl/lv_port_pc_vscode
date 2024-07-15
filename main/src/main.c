5a0cff04b51b000236de02f378a4209da975ad93
Jaime Lee <18038163520@163.com>
2024-07-15 16:50:43 +0800

    Update main.c

--- main/src/main.c
+++ main/src/main.c
@@ -54,11 +54,11 @@ int main(int argc, char **argv)
   lv_obj_t * scr = lv_scr_act();
 
   /* 创建一个用于放置方块的网格 */
-  static lv_coord_t col_dsc[] = {100, 20, 100, LV_GRID_TEMPLATE_LAST};  // 每个方块100px，间隙20px
-  static lv_coord_t row_dsc[] = {100, 20, 100, 20, 100, LV_GRID_TEMPLATE_LAST};  // 每个方块100px，间隙20px
+  static lv_coord_t col_dsc[] = {80, 10, 80, LV_GRID_TEMPLATE_LAST};  // 每个方块80px，间隙10px
+  static lv_coord_t row_dsc[] = {80, 10, 80, 10, 80, LV_GRID_TEMPLATE_LAST};  // 每个方块80px，间隙10px
   
   lv_obj_t * grid = lv_obj_create(scr);
-  lv_obj_set_size(grid, 220, 340);  // 宽度为2个100px的方块和1个20px的间隙，高度为3个100px的方块和2个20px的间隙
+  lv_obj_set_size(grid, 240, 320);  // 宽度为240px，高度为320px
   lv_obj_center(grid);
   lv_obj_set_layout(grid, LV_LAYOUT_GRID);
   lv_obj_set_grid_dsc_array(grid, col_dsc, row_dsc);
@@ -73,7 +73,7 @@ int main(int argc, char **argv)
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 2; j++) {
       lv_obj_t * block = lv_btn_create(grid);
-      lv_obj_set_size(block, 100, 100);  // 设置大小为100px x 100px
+      lv_obj_set_size(block, 80, 80);  // 设置大小为80px x 80px
       lv_obj_add_style(block, &style_block, 0);
       lv_obj_set_grid_cell(block, LV_GRID_ALIGN_CENTER, j * 2, 1, LV_GRID_ALIGN_CENTER, i * 2, 1);
     }
